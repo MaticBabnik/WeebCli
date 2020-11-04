26f2e1917d9571da2815a6f058ab2622567423a4
Matic Babnik <matic.babnik@outlook.com>
2020-11-04 14:46:24 +0100

    v 0.1.0
    initial commit

---
+++ WeebCli/PngParser.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+
+namespace WeebCli
+{
+    /// <summary>
+    /// Code foucsed on splitting ffmpeg's piped image output into individual images.
+    /// </summary>
+    public static class PngParser
+    {
+        private static readonly byte[] PngHeader = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
+        private static readonly byte[] PngEnd = { 73, 69, 78, 68 };
+        private const ushort PngMinChunkSize = 12;
+        /// <summary>
+        /// Splits ffmpeg piped output into induvidual files
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static List<Image> ParseFrames(FList<byte> bytes)
+        {
+            var images = new List<Image>();
+            var rb = bytes.array;
+
+            Console.Write("Frames: {0}", images.Count);
+
+            for (uint i = 0; i < rb.Length; i++) //this loop exist just in case ffmpeg puts trash between frames/files.
+            {
+                if (ArrayCompare(rb, i, i + PngHeader.Length, PngHeader))
+                {
+                    Console.CursorLeft = 0;
+                    Console.Write("Frames: {0}", images.Count);
+
+                    uint currentPngStart = i;
+
+                    i += (uint)PngHeader.Length;
+
+                    while (i < rb.Length) //this loop walks the whole png untill Iend chunk
+                    {
+                        uint chunkSize = PngMinChunkSize;
+
+                        chunkSize += (uint)(rb[i] << 24 | rb[i + 1] << 16 | rb[i + 2] << 8 | rb[i + 3]);
+
+                        if (chunkSize == 12 && ArrayCompare(rb, i + 4, i + 8, PngEnd))
+                        {
+                            images.Add(ImageFromBytes(ref rb, currentPngStart, i));
+
+                            i += chunkSize - 1; //-1 compensates for the +1 at the end of top level for loop
+
+                            break;
+                        }
+                        i += chunkSize;
+                    }
+                }
+            }
+            Console.WriteLine();
+            return images;
+        }
+        /// <summary>
+        /// Parses an image from raw byte data.
+        /// </summary>
+        /// <param name="bytes">Byte array containing atleast one png.</param>
+        /// <param name="start">First byte position of a PNG file.</param>
+        /// <param name="end">Last byte position of a PNG file.</param>
+        /// <returns>ImageSharp image</returns>
+        public static Image ImageFromBytes(ref byte[] bytes, uint start, uint end)
+        {
+            byte[] data = new byte[end - start];
+            Array.Copy(bytes, start, data, 0, end - start);
+            return Image.Load(data, new PngDecoder());
+        }
+        /// <summary>
+        /// Converts 4 bytes from an array to uint
+        /// </summary>
+        /// <param name="data">Source array</param>
+        /// <param name="offset">Most significant byte position</param>
+        /// <returns>unsigned int</returns>
+        public static uint BytesToUint(byte[] data, int offset = 0)
+        {
+            uint output = 0;
+            for (int i = offset, shift = 24; i < offset + 4; i++, shift -= 8)
+                output += (uint)(data[i] << shift);
+
+            return output;
+        }
+        /// <summary>
+        /// Compares a section of an arrray with another array.
+        /// </summary>
+        /// <typeparam name="T">The type of both arrays. Must implement IComparable</typeparam>
+        /// <param name="a">First array</param>
+        /// <param name="start">First array section start</param>
+        /// <param name="end">First array section end</param>
+        /// <param name="b">Second array</param>
+        /// <returns>True if equal</returns>
+        public static bool ArrayCompare<T>(T[] a, long start, long end, T[] b) where T : IComparable
+        {
+            for (long i = start; i < end; i++)
+            {
+                if (!a[i].Equals(b[i - start])) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ WeebCli/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using CommandLine;
+using System.Linq;
+using System.Diagnostics;
+using SixLabors.ImageSharp;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
+
+
+namespace WeebCli
+{
+    class Program
+    {
+
+        private static readonly string AppRoot = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        private static readonly string[] AcceptedExtensions = {"mkv","mp4"};
+
+        /// <summary>
+        /// Returns "\ffmpeg.exe" on windows and "\ffmpeg" on unix
+        /// </summary>
+        public static string FfmpegName
+        {
+            get
+            {
+                return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"ffmpeg\ffw.exe" : RuntimeInformation.IsOSPlatform(OSPlatform.Linux)? @"ffmpeg/ffl": @"ffmpeg/ffm";
+            }
+        }
+
+
+        [Verb("pregen", HelpText = "Generates previews")]
+        class PreGenOptions
+        {
+            [Option('i', "input", Required = true, HelpText = "File to open/generate previews for")]
+            public string Input { get; set; }
+
+            [Option('f', "frequency", Default = "1", Required = false, HelpText = "Preview frequency. -f 1/2 means that one frame will be previewed for 2 seconds.")]
+            public string Frequency
+            {
+                get;
+                set;
+            }
+
+            [Option('w', "width", Default = -2, Required = false, HelpText = "Preview frame width. Use -2 to keep aspect ratio.")]
+            public int Width { get; set; }
+
+            [Option('h', "height", Default = 60, Required = false, HelpText = "Preview frame height. Use -1 to keep aspect ratio.")]
+            public int Height { get; set; }
+        }
+
+        static int Main(string[] args)
+        {
+            
+            #region splash
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("_________________________________");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("_ _ _ ____ ____ ___  ____ _    _ \n| | | |___ |___ |__] |    |    | \n|_|_| |___ |___ |__] |___ |___ | ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("_________________________________\n");
+            Console.ResetColor();
+            #endregion
+
+            if (!File.Exists(Path.Combine(AppRoot, FfmpegName)))
+            {
+                PrintAndExit("No FFMPEG", ConsoleColor.Red, -1);
+            }
+
+            return CommandLine.Parser.Default.ParseArguments<PreGenOptions>(args).MapResult(
+                (PreGenOptions opts) => PreviewGen(opts),
+                errs => 1
+            );
+        }
+        static int PreviewGen(PreGenOptions opts)
+        {
+            if (!File.Exists(opts.Input)) { Console.WriteLine("File does not exist"); return 2; }
+            if (!AcceptedExtensions.Contains(opts.Input.Split('.').Last())) { Console.WriteLine("File format not supported"); return 3; }
+
+            var ffmpeg = Process.Start(
+               new ProcessStartInfo()
+               {
+                   WorkingDirectory = Environment.CurrentDirectory,
+                   UseShellExecute = false,
+                   FileName = Path.Combine(AppRoot, FfmpegName),
+                   RedirectStandardOutput = true,
+                   Arguments = $"-hide_banner -loglevel error -stats -i {opts.Input} -vf \"scale={opts.Width}:{opts.Height}, fps={opts.Frequency}\" -c:v png -f image2pipe -"
+               });
+
+            var bytes = ReadStdOut(ffmpeg);
+
+            var imgs = PngParser.ParseFrames(bytes);
+
+            WriteOutput(imgs);
+
+            return 0;
+        }
+        static FList<byte> ReadStdOut(Process p)
+        {
+            FList<byte> bytes = new FList<byte>();
+            while (true)
+            {
+                int a = p.StandardOutput.BaseStream.ReadByte();
+                if (a != -1) bytes.Add((byte)a);
+                if (a == -1 && p.HasExited) break;
+            }
+            return bytes;
+        }
+
+        static void WriteOutput(List<Image> images)
+        {
+            int w = images[0].Width, h = images[0].Height;
+
+            JpegEncoder enc = new JpegEncoder() { Quality = 20 };
+            for (int i = 0; i < images.Count; i += 100)
+            {
+                var img = new Image<Rgb24>(w * 10, h * 10);
+
+                for (int y = 0; y < 10 && i + y * 10 < images.Count; y++)
+                    for (int x = 0; x < 10 && i + y * 10 + x < images.Count; x++)
+                    {
+                        img.Mutate(im => im.DrawImage(images[i + y * 10 + x], new Point(x * w, y * h), 1f));
+                    }
+                img.SaveAsJpeg($"pr{i}.jpeg");
+                Console.WriteLine("Wrote map: pr{0:D3}.jpeg", i);
+            }
+        }
+
+        /// <summary>
+        /// Prints colored text to stdout and exits.
+        /// </summary>
+        /// <param name="text">The text to display</param>
+        /// <param name="color">Text color</param>
+        /// <param name="code">Return code</param>
+        static void PrintAndExit(string text, ConsoleColor color = ConsoleColor.White, int code = 0)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Environment.Exit(code);
+
+        }
+
+    }
+}
